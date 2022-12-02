dd409060e848722b124729b248108308ab2d7f22
Anthony Moran <tonymmoran@gmail.com>
2022-12-02 15:15:53 +0000

    completed the Boolean testss

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,73 @@
 function negate(a) {
-  // your code here
+  return !a;
 };
 
 function both(a, b) {
-  // your code here
+ return a && b;
 };
 
 function either(a, b) {
-  // your code here
+  return a || b;
 };
 
 function none(a, b) {
-  // your code here
+return !a && !b;
 };
 
 function one(a, b) {
-  // your code here
+return a != b;
 };
 
 function truthiness(a) {
-  // your code here
-};
+if (a) return true; 
+  return false
+}; 
 
 function isEqual(a, b) {
-  // your code here
+  return a === b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
-  // your code here
+  if (a%2 != 0) 
+    return true;
+    return false;
+
 };
 
 function isEven(a) {
-  // your code here
+  if (a%2 ==0)
+    return true;
+    return false;
 };
 
 function isSquare(a) {
-  // your code here
+  if(Number.isInteger(Math.sqrt(a))) return true;
+    return false;
 };
 
 function startsWith(char, string) {
-  // your code here
+  if(char == string.charAt(0)) return true;
+    return false;
 };
 
 function containsVowels(string) {
-  // your code here
-};
+  if(string.includes('a') || string.includes('e') || string.includes('O')) return true;
+   return false;
+  
+  }
+
 
 function isLowerCase(string) {
-  // your code here
+  if (string.toLowerCase() === string) return true;
+  return false;
 };
 
 module.exports = {
